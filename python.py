6ef0bceda17cc6fb61528be0289385935cfe26db
nannoahmed <af25102002@gmail.com>
2025-09-14 21:11:30 +0300

    First Edit For Python File

--- python.py
+++ python.py
@@ -1,2 +1,12 @@
-# This Is First Comment
-print ("Hello Ahmed")
+# # This Is First Comment
+# print ("Hello Ahmed")
+
+# help("keywords")
+
+# msg = "I Love"
+# lang = "Python"
+# print(msg + " " + lang)
+
+# full = msg + " " + lang
+# print(full)
+
